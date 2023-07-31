aed789d77cb9476badfeea9ed3e202d06e8e713d
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-07-31 10:32:17 -0300

    Adicionar o arquivo RoundImage.

---
+++ Contact/View/Shared/RoundImage.swift
@@ -0,0 +1,31 @@
+//
+//  RoundImage.swift
+//  Contact
+//
+//  Created by Rafael Gonzaga on 31/07/23.
+//
+
+import SwiftUI
+
+struct RoundImage: View {
+    
+    let image: UIImage
+    
+    var body: some View {
+        
+    Image(uiImage: image)
+        
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .clipShape(Circle())
+            .frame(width: 200, height: 200)
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 24)
+    }
+}
+
+struct RoundImage_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundImage(image: UIImage(imageLiteralResourceName: "bruce"))
+    }
+}
