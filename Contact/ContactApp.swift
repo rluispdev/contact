b08404ad467c8b2fc5a09984d86b5167ebfd3357
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-07-31 09:39:46 -0300

    Adicionar o primeiro commit e o arquivo ContactModel

---
+++ Contact/ContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactApp.swift
+//  Contact
+//
+//  Created by Rafael Gonzaga on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contact/Model/ContactModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContactModel.swift
+//  Contact
+//
+//  Created by Rafael Gonzaga on 31/07/23.
+//
+
+import SwiftUI
+
+struct Contact {
+    
+    var photo: UIImage
+    var name: String
+    var position: String
+    var email: String
+    var phone: String
+    
+}
