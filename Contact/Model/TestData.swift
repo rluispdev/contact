fc5c0259bbf852051d20877569cff61effa48b0a
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-08-01 16:31:53 -0300

    Alteração no formato do modelo do numero no arquivo TestData

---
+++ Contact/Model/TestData.swift
@@ -0,0 +1,19 @@
+//
+//  TestData.swift
+//  Contact
+//
+//  Created by Rafael Gonzaga on 31/07/23.
+//
+
+import Foundation
+import UIKit
+
+struct TestData {
+    static let contact = Contact(
+        
+        photo: UIImage(imageLiteralResourceName: "bruce"),
+        name: "Bruce Wayne",
+        position: "Wayne Enterprises",
+        email: "bruce@wayne.com",
+        phone: "4-(968)705-1370")
+}
